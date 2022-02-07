905bef5f4be4a98360bc24c4a703efb4ef9334c9
KiyoshiAndres <89398401+KiyoshiAndres@users.noreply.github.com>
2022-02-08 01:13:02 +0900

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Kiyoshi Takeuchi on 2022/02/08.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
